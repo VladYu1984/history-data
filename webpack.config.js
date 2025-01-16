0fe0b759766f2bcb946a72b07b6992f8fb9220f8
Vlad <bobilev.v@fmedia.tech>
2025-01-16 16:36:43 +0700

    #build add gh-pages and create build version

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new htmlWebpackPlugin({
